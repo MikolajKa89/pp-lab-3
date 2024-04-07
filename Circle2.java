e36da2eb7b00413935075de7e2f22e0e166d933e
MikolajKa89 <164363596+MikolajKa89@users.noreply.github.com>
2024-04-07 12:08:04 +0200

    Create Circle2.java
    dodanie

---
+++ Circle2.java
@@ -0,0 +1,18 @@
+package geometry;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Point center = new Point(1, 2);
+        double radius = 5;
+
+        Circle circle = new Circle(center, radius);
+
+        System.out.println("Właściwości koła:");
+        System.out.println("Środek: " + circle.getCenter());
+        System.out.println("Promień: " + circle.getRadius());
+        System.out.println("Obwód: " + circle.calculatePerimeter());
+        System.out.println("Pole powierzchni: " + circle.getArea());
+    }
+
+}
